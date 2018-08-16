6c9c9f3444ea4ca63470659756c2621c3c525676
johnstonstu <stu.w.johnston@gmail.com>
2018-08-16 14:16:25 -0700

    created all tests to run on vending machine, without expects

--- __tests__/use-vending-machine.spec.js
+++ __tests__/use-vending-machine.spec.js
@@ -4,67 +4,59 @@ const vendingMachine = require("../src/vending-maching.js");
 /**
  * -total items => list
  * -single item => single item
- * -items not in stock => list
  * -items in stock => list
- * -buy item exact change => success, remove 1 stock
+ * -insert $ => update credit
+ * -buy item exact change => success, remove 1 stock, reset credit
  * -buy item not enough $ => error
- * -buy item and give change => success, remove 1 stock, give change
+ * -buy item and give change => success, remove 1 stock, give change, reset credit
  * -buy item not in stock => error
  * -buy item > $10 bill => error
- *
- * -add stock => success
- * -add float => success
  */
 
-describe("vendingMachine", () => {
-  describe("when a customer wants to use the vending machine", () => {
-    describe("when a worker wants to see all items", () => {
-      it("should return a list of items, with price and current stock", () => {
-        expect(data).toEqual({
-          items: {
-            choclate: {
-              price: 1,
-              stock: 5
-            },
-            pop: {
-              price: 1,
-              stock: 5
-            },
-            choclate: {
-              price: 1,
-              stock: 0
-            },
-            pop: {
-              price: 1,
-              stock: 0
-            }
-          }
-        });
-      });
-    });
-    describe("when a worker wants to see what items are in stock", () => {
-      it("should return a list of items in stock, with stock remaining", () => {
-        expect(data.inStock).toEqual({
-          items: {
-            choclate: {
-              stock: 5
-            },
-            pop: {
-              stock: 5
-            }
-          }
-        });
-      });
-    });
-    describe("when a worker wants to see what items are not in stock", () => {
-      it("should return a list of items in stock", () => {
-        expect(data.noStock).toEqual({
-          items: ["chips"]
-        });
-      });
+describe("when a customer is using the vending maching", () => {
+  describe("when a customer wants to see all the items", () => {
+    it("should return a list of all the items", () => {
+      expect().toEqual();
     });
   });
-  describe("when doing maintainence on the vending machine", () => {
-    describe("when a customer wants info on a specific item", () => {});
+  describe("when a customer wants to see items in stock", () => {
+    it("should return a single item", () => {
+      expect().toEqual();
+    });
+  });
+  describe("when a customer wants to see a single item", () => {
+    it("should return a single item", () => {
+      expect().toEqual();
+    });
+  });
+  describe("when a customer inserts money", () => {
+    it("should update credit", () => {
+      expect().toEqual();
+    });
+  });
+  describe("when a customer buys an item with exact change", () => {
+    it("should return success remove 1 stock of item, reset credit", () => {
+      expect().toEqual();
+    });
+  });
+  describe("when a customer buys an item with more credit then the cost", () => {
+    it("should return success remove 1 stock of item, return credit in change from float", () => {
+      expect().toEqual();
+    });
+  });
+  describe("when a customer does not have enouch credit to buy item", () => {
+    it("should return failure", () => {
+      expect().toEqual();
+    });
+  });
+  describe("when a customer tries to buy item not in stock", () => {
+    it("should return failure", () => {
+      expect().toEqual();
+    });
+  });
+  describe("when a customer tries to use > $10 bill", () => {
+    it("should return failure", () => {
+      expect().toEqual();
+    });
   });
 });
